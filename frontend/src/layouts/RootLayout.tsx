305d629e9704804fb378965ba7648db47a551955
MG2003 <max.guo2014@gmail.com>
2023-07-19 16:44:55 -0400

    Navigation implemented

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,16 +1,17 @@
 
 
-import { Outlet } from "react-router-dom";
+import { Outlet, Link, NavLink } from "react-router-dom";
+import { pages } from "../utils/pages";
 
 
 export default function Root() {
     return (
       <div>
         <header>
-          <h1 className = "text-3xl font-bold">Max Guo</h1>
+          <h1 className = "text-3xl font-bold"><Link to= "/">Max Guo</Link></h1>
           <p className = "font-semibold">hms is cool</p>
-  
         </header>
+        <Navigation/>
         <main className = "m-24 p-16 border-4 border-solid border-black">
           <Outlet/>
         </main>
@@ -25,5 +26,24 @@ export default function Root() {
       <footer>
         <p>Made with React and Tailwind</p>
       </footer>
+    )
+  }
+
+  function Navigation(){
+    return(
+      <ul>
+        {pages.map((page, index) => 
+          <NavLink 
+            key = {index} 
+            to={page.path}
+            className = {({ isActive }) =>
+              isActive ? "font-bold" : ""
+          }
+            >
+
+              {page.title}
+
+          </NavLink>)}
+      </ul>
     )
   }
---
+++ frontend/src/utils/pages.tsx
@@ -0,0 +1,14 @@
+export const pages:  { title: string, path: string}[] = [
+    {
+        title: "about me",
+        path: "/about"
+    },
+    {
+        title: "experience",
+        path: "/experience"
+    },
+    {
+        title: "projects",
+        path: "/projects"
+    }
+]
